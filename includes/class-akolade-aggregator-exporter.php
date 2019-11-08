1b40c6c49c04325e0a0d0b436e775a71fae51f7a
Sudip Limbu <responsivesudip@gmail.com>
2019-11-08 07:57:04 +0545

    Add exporter and importer class

---
+++ includes/class-akolade-aggregator-exporter.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ak_Exporter
+{
+    // should be able to hook into save/update post
+    // Collect post info
+    // push to network sites
+    // Encrypt data
+}
---
+++ includes/class-akolade-aggregator-importer.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ak_Importer
+{
+    // should expose an endpoint where Ak_Exporter can make a call
+    // Decrypt data
+    // validate and save raw data
+    // Extract raw data and save posts including
+}
